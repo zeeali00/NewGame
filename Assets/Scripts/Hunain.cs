3ed4ec6a4cbf87e0912ecc91571613aec92447a3
zeeali00 <36164635+zeeali00@users.noreply.github.com>
2020-08-22 03:06:58 +0500

    5th
    Created Script Called Hunain

--- Assets/Scripts/Hunain.cs
+++ Assets/Scripts/Hunain.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewScript : MonoBehaviour
+public class Hunain : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.Translate(Vector3.up);
     }
 }
