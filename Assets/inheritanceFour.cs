c9332ed65c0edbf4d4ec6c69d95431082a804d8d
zeeali00 <36164635+zeeali00@users.noreply.github.com>
2020-08-24 21:12:52 +0500

    TodaysChanges912

---
+++ Assets/inheritanceFour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inheritanceFour : bac
+{
+    // Start is called before the first frame update
+    public bac Parent;
+
+    
+
+    inheritanceFour() 
+    {
+      
+    }
+
+
+    [ContextMenu("Start")]
+    void Start()
+    {
+        bac bac = new bac();
+        bac.objecte = this.objecte;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/inheritanceOne.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inheritanceOne : bac
+{
+    // Start is called before the first frame update
+    [ContextMenu("Start")]
+    void Start()
+    {
+        objecte = GameObject.Find("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
